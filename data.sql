39ec0d77eb9e44615787bcb6e9f8cc15216b9514
francksefu <francksefu1998@gmail.com>
2023-08-16 18:10:03 +0200

    Add picture

--- data.sql
+++ data.sql
@@ -14,7 +14,7 @@ VALUES('Devimon', '2017-05-12', 5, TRUE, 11);
 ALTER TABLE animals ADD species VARCHAR(50);
 
 INSERT INTO animals (name, date_of_birth, espace_attempts, neutered, weight_kg)
-values('Charmander', '2020-02-08', 0, FALSE, -11),('Plantmon', '2021-11-15', 2, TRUE, -5.7)
+values('Charmander', '2020-02-08', 0, FALSE, -11),('Plantmon', '2021-11-15', 2, TRUE, -5.7);
 
 INSERT INTO animals (name, date_of_birth, espace_attempts, neutered, weight_kg)
 VALUES ('Squirtle', '1993-04-02', 3, FALSE, -12.13), ('Angemon', '2005-06-12', 1, TRUE, -45), ('Boarmon', '2005-06-07', 7, TRUE, 20.4);
